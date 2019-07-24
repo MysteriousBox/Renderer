b8ef169eab1263a7622dc18144254be860077499
John <yangzhenzhuozz@126.com>
2019-07-24 12:53:01 +0800

    a

--- Graphics.cpp
+++ Graphics.cpp
@@ -6,9 +6,13 @@
 #include <io.h>
 #include <algorithm>
 #pragma warning(disable:4996)
-//定义了多边形的任意两条边不会交叉，这样就不用在每条扫描线中对其边表排序，当然如果允许两条边交叉的话取消这个宏定义的话就可以
-	//因为我知道三角形肯定不会相交，而且我绘制的都是三角形，所以才定义了这个宏，如果使用本函数绘制其他多边形，比如五边形，六边形等，如果运行两条边相交，则应当取消_EdgeNotCross宏，即 #undef _EdgeNotCross
+
+
+//宏_EdgeNotCross 定义了多边形的任意两条边不会交叉，这样就不用在每条扫描线中对其边表排序，当然如果允许两条边交叉的话取消这个宏定义的话就可以
+//因为我知道三角形肯定不会相交，而且我绘制的都是三角形，所以才定义了这个宏，如果使用本函数绘制其他多边形，比如五边形，六边形等，如果运行两条边相交，则应当取消_EdgeNotCross宏，即 #undef _EdgeNotCross
 #define _EdgeNotCross
+
+
 
 bool Graphics::setViewPort(int x, int y, int w, int h)
 {
